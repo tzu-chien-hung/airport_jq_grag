efa3364ff3f353737688cde927295721a92067b5
tzu-chien-hung <tina19960522@gmail.com>
2022-07-04 11:12:29 +0800

    add icon img

--- jq_drag/index.js
+++ jq_drag/index.js
@@ -91,10 +91,10 @@ function handleComplete(evt,comp) {
 		})
 	});
 	nav_introduce.addEventListener("click",()=>{
-		popNavText.innerHTML ='';
+		popNavText.innerHTML ='<div class="popup_nav_form_text"><div class="popup_nav_img"><img src="../img/t3-plan1.jpeg" alt=""></div><div class="popup_nav_word_all"><div class="popup_nav_word">• 桃園國際機場啟用40多年，在服務與設備升級的同時，更兼顧環保策略，實踐永續經營理念，推動機場節能減碳，預定在2050年達到淨零排放的目標。</div><div class="popup_nav_word">• 機場節能減碳在航廈設施、機坪作業車輛、水資源管理和焚化爐升級皆有具體作為。減碳成果持續獲獎，榮獲ISO 14064-1溫室氣體盤查、ISO 50001能源管理系統、ISO 14001環境管理系統，並連續4年獲得ACI綠色機場肯定。</div><div class="popup_nav_word">• 讓我們一起認識桃園機場綠色作為吧!</div></div></div>';
     });
 	nav_outlook.addEventListener("click",()=>{
-		popNavText.innerHTML ='';
+		popNavText.innerHTML ='<div class="popup_nav_form_text"><div class="popup_nav_img popup_nav_img2"><img src="./images/image_future.png" alt=""></div><div class="popup_nav_word_all"><div class="popup_nav_word"><h4>• 近期四大目標</h4><div style="margin-left: 20px;">-水足跡盤查<br>-空、陸側充電樁由現有 54 座增設至 180 座<br>-第三航廈建置太陽能發電機組<br>-評估購置再生能源或設置儲能設備可行性</div></div><div class="popup_nav_word"><h4>• 2025年前，取得機場碳認證（ACA）等級四之認證</h4></div><div class="popup_nav_word"><h4>• 在2050 年達到淨零排放的目標</h4></div></div></div>';
     });
     closeNav.addEventListener("click",()=>{
 		document.getElementById("popupNavBg").style.display = "none";
