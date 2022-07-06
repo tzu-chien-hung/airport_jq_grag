b7d727a88925702e4bbbd8bf74ae783674236986
tzu-chien-hung <tina19960522@gmail.com>
2022-07-06 10:22:41 +0800

    fix nav 活動專區連結

--- jq_drag/js/index.js
+++ jq_drag/js/index.js
@@ -102,6 +102,7 @@ function handleComplete(evt, comp) {
 	const popNavText = document.getElementById("popNavText");
 	const checked_btn = document.getElementById("nav-toggle");
 	const closeNav = document.getElementById("closeNav");
+	const activity_btn = document.getElementById("navTitleEnd");
 	popNavAll.forEach((el) => {
 		el.addEventListener("click", () => {
 			checked_btn.checked = false;
@@ -122,7 +123,10 @@ function handleComplete(evt, comp) {
 		document.getElementById("popupNavBg").style.display = "none";
 		document.getElementById("popupNavContent").style.display = "none";
 	});
-
+	activity_btn.addEventListener("click",() => {
+		location.href="https://tzu-chien-hung.github.io/airport_jq_grag/jq_drag/activity.html"
+	});
+	
 	var
 		//Registers the "tick" event listener.
 		fnStartAnimation = function () {
