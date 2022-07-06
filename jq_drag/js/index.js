27f3227462a1a78ddb6e6a629f22b7f8171ffbe8
tzu-chien-hung <tina19960522@gmail.com>
2022-07-06 15:10:15 +0800

    fix js

--- jq_drag/js/index.js
+++ jq_drag/js/index.js
@@ -123,7 +123,14 @@ function handleComplete(evt, comp) {
 		document.getElementById("popupNavBg").style.display = "none";
 		document.getElementById("popupNavContent").style.display = "none";
 	});
-
+	const activity_btn = document.getElementById("navTitleEnd");
+	const activity_btn_bear = document.getElementById("gameBlock");
+	activity_btn.addEventListener("mousedown", () => {
+		location.href = "https://tzu-chien-hung.github.io/airport_jq_grag/jq_drag/activity.html"
+	});
+	activity_btn_bear.addEventListener("mousedown", () => {
+		location.href = "https://tzu-chien-hung.github.io/airport_jq_grag/jq_drag/activity.html"
+	});
 
 	var
 		//Registers the "tick" event listener.
