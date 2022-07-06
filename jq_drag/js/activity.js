86de5360304dea77d846ffa74cd424e7e3a70c3b
tzu-chien-hung <tina19960522@gmail.com>
2022-07-06 16:38:17 +0800

    fix js

--- jq_drag/js/activity.js
+++ jq_drag/js/activity.js
@@ -1,7 +1,7 @@
 
 
 const activity_main1_btn = document.getElementById("activityMain1Btn");
-console.log(activity_btn_bear)
+console.log(activity_main1_btn);
 
 activity_main1_btn.addEventListener("mousedown", () => {
     location.href = "https://tzu-chien-hung.github.io/airport_jq_grag/jq_drag/game.html"
